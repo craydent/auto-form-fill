d759ee41131258525ac29d0fdebfffa727c9e1ee
Clark Inada <akkun1983@gmail.com>
2019-08-15 20:08:52 -0700

    added populate

--- autoFormFill/scripts/popup.js
+++ autoFormFill/scripts/popup.js
@@ -4,7 +4,19 @@ function generateURLFromDOMForAutoFormFill() {
       tabs[0].id,
       { action: "generateURLFromDOMForAutoFormFill" },
       function () {
-        debugger;
+        var el = document.querySelector('.checkmark');
+        el.className = "checkmark";
+        el.style.opacity = 1;
+        setTimeout(fade, 700);
+      });
+  });
+}
+function populateFromURL() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "populateFromURL" },
+      function () {
         var el = document.querySelector('.checkmark');
         el.className = "checkmark";
         el.style.opacity = 1;
@@ -14,8 +26,14 @@ function generateURLFromDOMForAutoFormFill() {
 }
 function fade() {
   var el = document.querySelector('.checkmark');
-  if (el.style.opacity <= 0) { return; }
+  if (el.style.opacity <= 0) {
+    var el = document.querySelector('.checkmark');
+    el.className = "checkmark hide";
+    return;
+  }
   el.style.opacity -= 0.1
   setTimeout(fade, 100);
 }
+
+document.getElementById("btnPopulate").addEventListener('click', populateFromURL);
 document.getElementById("btnGenerate").addEventListener('click', generateURLFromDOMForAutoFormFill);
